a680e43981f2ffa23c3a20e4ed2de33d66084fd6
RohitCU18 <rohit1818.cse18@chitkara.edu.in>
2021-03-31 18:15:42 +0530

    ST2 BACKEND UPGRAD

---
+++ src/main/java/com/example/Backendst2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendst2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backendst2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Controller/PostController.java
@@ -0,0 +1,4 @@
+package com.example.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
